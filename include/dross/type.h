58d96608bfded743ab0a5a2d2c791a2dc2e339f2
Yuma Endo <endo@skipbit.jp>
2024-10-26 09:42:17 +0900

    Add concat function

--- include/dross/type.h
+++ include/dross/type.h
@@ -9,6 +9,54 @@
 
 namespace dross {
 
+/**
+ * @brief Split a string into tokens.
+ *
+ * @param s The string to split.
+ * @param delimiter The delimiter to split on.
+ * @return std::vector<std::string> A vector of tokens.
+ */
 std::vector<std::string> split(const std::string& s, const char& d);
+
+/**
+ * @brief Concatenate containers.
+ *
+ * @tparam T The container type.
+ * @param a The container to concatenate.
+ * @return T The concatenated container.
+ */
+template <typename T> concept container_type = requires(T a) {
+    { a.begin() } -> std::same_as<typename T::iterator>;
+    { a.end() } -> std::same_as<typename T::iterator>;
+    requires std::is_same_v<decltype(a.insert(a.end(), *a.begin())), typename T::iterator>;
+};
+
+/**
+ * @brief Concatenate containers.
+ *
+ * @tparam T The container type.
+ * @param a The container to concatenate.
+ * @return T The concatenated container.
+ */
+template <container_type T>
+T concat(const T& a) {
+    return a;
+}
+
+/**
+ * @brief Concatenate containers.
+ *
+ * @tparam T The container type.
+ * @tparam Ts The container types.
+ * @param a The container to concatenate.
+ * @param b The container to concatenate.
+ * @return T The concatenated container.
+ */
+template<container_type T, typename... Ts>
+T concat(const T& a, const Ts&... b) {
+    T c = concat(b...);
+    c.insert(c.end(), a.begin(), a.end());
+    return c;
+}
 
 }
--- test/type/test_type.cpp
+++ test/type/test_type.cpp
@@ -11,3 +11,11 @@ TEST(string_split_test, colon_delimited)
     EXPECT_EQ(v[1], "bar");
     EXPECT_EQ(v[2], "baz");
 }
+
+TEST(concat_test, two_number_vector_concat)
+{
+    const std::vector<int> a = { 1, 2, 3 };
+    const std::vector<int> b = { 4, 5, 6 };
+    const auto c = dross::concat(a, b);
+    ASSERT_EQ(c, std::vector<int>({ 1, 2, 3, 4, 5, 6 }));
+}
